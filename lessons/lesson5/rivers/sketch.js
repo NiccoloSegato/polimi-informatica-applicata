ef89c9f1affd79c59b5981f11b062e90ce5c7d32
niccolosegato <segato117@gmail.com>
2024-11-16 07:49:38 +0100

    Assignment 3 started

---
+++ lessons/lesson5/rivers/sketch.js
@@ -0,0 +1,89 @@
+let riversData;
+let data;
+let datasetLength;
+let columnWidth;
+let highestLength;
+
+function preload() {
+  data = loadTable("data/rivers-data.csv", "csv", "header");
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  columnWidth = (windowWidth - 20 - (5 * datasetLength)) / datasetLength;
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  riversData = data.getObject();
+  datasetLength = Object.keys(riversData).length;
+  columnWidth = (windowWidth - 20 - (5 * datasetLength)) / datasetLength;
+
+  highestLength = windowHeight - 300;
+}
+
+function draw() {
+  background(220);
+  noStroke();
+
+  let hoveredRiverIndex = floor(floor(mouseX / columnWidth) / 5);
+  let hoveredRiver = riversData[hoveredRiverIndex] ?? riversData[0];
+
+  // Title
+  textSize(15);
+  fill("black");
+  textStyle(NORMAL);
+  text("RIVERS", 20, 30);
+
+  // River name
+  textSize(50);
+  textStyle(BOLD);
+  fill("#FC6900");
+  text(hoveredRiver.name, 20, 80);
+
+  // River length
+  textSize(15);
+  textStyle(NORMAL);
+  fill("black");
+  text("Length: " + hoveredRiver.length + " km", 20, 110);
+  textSize(15);
+
+  text("Hovered Index = " + hoveredRiverIndex, 20, 170);
+
+  // River temperatures
+  text("Temperature: " + hoveredRiver.min_temp + "°C - " + hoveredRiver.max_temp + "°C", 20, 150);
+
+  // River countries
+  text("Countries: " + hoveredRiver.countries, 20, 130);
+
+  for(let i = 0; i < datasetLength; i++) {
+    let row = riversData[i];
+    let x = i * columnWidth;
+
+    drawColumn(10+ x + 5 * i, row);
+  }
+
+  fillHoveredColumn(hoveredRiver);
+}
+
+function drawColumn(x, data) {
+  let length = data.length;
+  let mappedLength = map(length, 60, 6700, 10, highestLength);
+
+  fill("grey");
+  rect(x, windowHeight, columnWidth, -mappedLength);
+  fill("black");
+}
+
+function fillHoveredColumn(hoveredRiver) {
+  mappedLength = map(hoveredRiver.length, 60, 6700, 10, highestLength);
+  let hoveredIndex = floor(floor(mouseX / columnWidth) / 2);
+  fill("#FC6900");
+  if(hoveredIndex >= 0 && hoveredIndex < datasetLength) {
+    rect(10 + (hoveredIndex * columnWidth) + 5 * hoveredIndex, windowHeight, columnWidth, -mappedLength);
+  }
+  else {
+    rect(10, windowHeight, columnWidth, -mappedLength);
+  }
+}
